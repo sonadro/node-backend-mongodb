cbdd3229705268d24bc7a728836dc88a57d22d53
sonadro <sondre.rooth@gmail.com>
2023-04-18 13:35:59 +0200

    Update formController to remove unused if-check

--- controllers/formController.js
+++ controllers/formController.js
@@ -7,21 +7,19 @@ const { formDocument } = require('../models/form');
 module.exports.form_post = (req, res) => {
     const data = req.body;
 
-    if (data.parcel) {
-        const document = new formDocument(data.parcel);
-        
-        // save document
-        try {
-            document.save();
-            res.status(200).send({
-                status: 'Document has been uploaded'
-            });
-        } catch (err) {
-            console.error(err);
-            res.status(400).send({
-                status: `error: ${err}`
-            });
-        }
+    const document = new formDocument(data.parcel);
+    
+    // save document
+    try {
+        document.save();
+        res.status(200).send({
+            status: 'Document has been uploaded'
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(400).send({
+            status: `error: ${err}`
+        });
     };
 };
 
@@ -30,19 +28,17 @@ module.exports.form_readOne = async (req, res) => {
     
     const docId = data.id;
 
-    if (data) {
-        try {
-            const document = await formDocument.findOne({ _id: docId });
-            res.status(200).send({
-                status: 'Document has been read',
-                document
-            });
-        } catch (err) {
-            console.error(err);
-            res.status(400).send({
-                status: `error: ${err}`
-            });
-        };
+    try {
+        const document = await formDocument.findOne({ _id: docId });
+        res.status(200).send({
+            status: 'Document has been read',
+            document
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(400).send({
+            status: `error: ${err}`
+        });
     };
 };
 
@@ -51,23 +47,21 @@ module.exports.form_updateOne = async (req, res) => {
 
     console.log(data);
 
-    if (data) {
-        try {
-            const oldDoc = await formDocument.findOne({ _id: data.id });
-            const update = data.document;
-            await oldDoc.updateOne(update);
+    try {
+        const oldDoc = await formDocument.findOne({ _id: data.id });
+        const update = data.document;
+        await oldDoc.updateOne(update);
 
-            const newDoc = await formDocument.findOne({ _id: data.id});
-            res.status(200).send({
-                status: 'Dokument has been updated',
-                document: newDoc
-            });
-        } catch(err) {
-            console.error(err);
-            res.status(400).send({
-                status: `error: ${err}`
-            });
-        };
+        const newDoc = await formDocument.findOne({ _id: data.id});
+        res.status(200).send({
+            status: 'Dokument has been updated',
+            document: newDoc
+        });
+    } catch(err) {
+        console.error(err);
+        res.status(400).send({
+            status: `error: ${err}`
+        });
     };
 };
 
@@ -76,42 +70,36 @@ module.exports.form_deleteOne = async (req, res) => {
 
     console.log(data);
 
-    if (data) {
-        try {
-            await formDocument.findOneAndRemove({ _id: data.id });
-            console.log('Deleted document');
-            res.status(200).send({
-                status: 'Document has been deleted'
-            });
-        } catch(err) {
-            console.error(err);
-            res.status(400).send({
-                status: `error: ${err}`
-            });
-        }
-    };
+    try {
+        await formDocument.findOneAndRemove({ _id: data.id });
+        console.log('Deleted document');
+        res.status(200).send({
+            status: 'Document has been deleted'
+        });
+    } catch(err) {
+        console.error(err);
+        res.status(400).send({
+            status: `error: ${err}`
+        });
+    }
 };
 
 module.exports.form_getAllIds = async (req, res) => {
-    const data = req.body;
+    try {
+        const allDocs = await formDocument.find({ });
+        let allIds = [];
+        allDocs.forEach(doc => {
+            allIds.push(doc._id.toString());
+        });
 
-    if (data) {
-        try {
-            const allDocs = await formDocument.find({ });
-            let allIds = [];
-            allDocs.forEach(doc => {
-                allIds.push(doc._id.toString());
-            });
-
-            res.status(200).send({
-                status: 'fetched all ids',
-                allIds
-            });
-        } catch(err) {
-            console.error(err);
-            res.status(400).send({
-                status: `error: ${err}`
-            });
-        };
+        res.status(200).send({
+            status: 'fetched all ids',
+            allIds
+        });
+    } catch(err) {
+        console.error(err);
+        res.status(400).send({
+            status: `error: ${err}`
+        });
     };
 };
